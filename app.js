673c81891fc513f8d7dd602de42edcb35ab16652
shivakumar <shivamahajan055@gmail.com>
2025-03-10 19:17:52 +0530

    added routes folder

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const productsRouter = require("./routes/products");
+const categoriesRouter = require("./routes/categories");
+
 const app = express();
 
 app.use((req, res, next) => {
@@ -8,21 +11,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/products", (req, res) => {
-  res.send(`<h1>Here is the list of all products.</h1>`);
-});
-
-app.post("/products", (req, res) => {
-  res.send(`<h1>A new product has been added.</h1>`);
-});
-
-app.get("/categories", (req, res) => {
-  res.send(`<h1>Here is the list of all categories.</h1>`);
-});
-
-app.post("/categories", (req, res) => {
-  res.send(`<h1>A new category has been created.</h1>`);
-});
+app.use("/products", productsRouter);
+app.use("/categories", categoriesRouter);
 
 app.get("*", (req, res) => {
   res.status(404).send(`<h1>404 - Page Not Found</h1>`);
