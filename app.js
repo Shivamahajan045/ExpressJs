c75ac7a9a37353c526e7fc56d98731c672399558
shivakumar <shivamahajan055@gmail.com>
2025-03-10 17:42:31 +0530

    server with routes

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const app = express();
 
-app.use("/welcome", (req, res, next) => {
-  req.user = "Guest";
-  next();
+app.get("/orders", (req, res) => {
+  console.log("Here is the list of all orders.");
+  res.send(`<h1>Here is the list of all orders.</h1>`);
 });
 
-app.get("/welcome", (req, res) => {
-  res.send(`<h1>Welcome ${req.user}</h1>`);
+app.post("/orders", (req, res) => {
+  console.log("A new order has been created.");
+  res.send(`<h1>A new order has been created.</h1>`);
+});
+
+app.get("/users", (req, res) => {
+  console.log("Here is the list of all users.");
+  res.send(`<h1>Here is the list of all users.</h1>`);
+});
+
+app.post("/users", (req, res) => {
+  console.log("A new user has been added.");
+  res.send(`<h1>A new user has been added</h1>`);
 });
 
 app.listen(3000, () => {
